c31b6a5ea43492ac5d3180084712610f226d3b2f
ttian99 <176553611@qq.com>
2016-08-03 21:09:25 +0800

    增加mongodb的连接

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
+// 初始化配置文件
+var cfg = require('./cfg.js');
+cfg.init('zqy');
+
+// 初始化express 
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+// 连接mongoDb数据库
+var mongo = require('./lib/db/initMongodb.js');
+mongo.connectMongo(cfg.mongo.uri, cfg.mongo.options);
+
+
+
+// 监听process的异常
+process.on('uncaughtException', function (e) {
+  // log.fatal('process Caught exception: ' + e.stack);
+  console.log('process Caught exception: ' + e.stack);
 });
 
-var server = app.listen(4000, function () {
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+  res.end();
+});
+
+// 服务器监听 
+var server = app.listen(8010, function () {
   var host = server.address().address;
   var port = server.address().port;
 
--- cfg.js
+++ cfg.js
@@ -1,7 +1,21 @@
 var cfg = {
-	init: function (){
+	isDev: false, // 是否在调试模式下
+  	dbname: null, // 数据库名称
+  	mongo: { uri: null, options: { db: { safe: true } } }, // mongo数据
+
+	init: function (dbname){
+		this.dbname = dbname;
+		this._initMongo();
 		console.log('this is init of cfg ！');
+	},
+
+	_initMongo: function() {
+		var dbUri = 'mongodb://localhost/';
+		if (this.isDev) {
+			dbUri = 'mongodb://192.168.1.176/';
+		}
+		this.mongo.uri = dbUri + this.dbname;
 	}
-}
+};
 
 module.exports = cfg;
